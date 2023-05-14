9600a4ba89301e1e4a5861fd1b7b110a52bedb47
albabernal03 <ALBABERO@MYUAX.COM>
2023-05-14 23:21:50 +0200

    FUNCIÓN QUE OBTIENE IMÁGENES DE UN DOCUMENTO HTML

--- generadores.py
+++ generadores.py
@@ -1,6 +1,7 @@
 #En primer lugar vamos a descargar las librerías 
 import asyncio #asyncio es la libreria principal de la programaión asíncrona y se utiliza para crear tareas asíncronas
 import aiohttp #aiohttp es una librería que permite realizar peticiones HTTP de forma asíncrona
+from bs4 import BeautifulSoup #BeautifulSoup es una librería que permite extraer datos de archivos HTML y XML
 
 async def wget(session, url): #async def define una función asíncrona
     async with session.get(url) as response: #async with permite ejecutar una función asíncrona dentro de otra
@@ -21,4 +22,8 @@ async def download(session,uri): #Función que descarga el contenido de una url
         return uri 
     
 
-    
+async def get_images_src_from_html(html_doc): #Función que obtiene las imágenes de un documento HTML
+    soup = BeautifulSoup(html_doc, "html.parser")  #Creamos un objeto BeautifulSoup a partir del documento HTML, um objeto BeautifulSoup es un árbol que representa el documento HTML
+    for img in soup.find_all('img'):  #Recorremos todas las etiquetas img del documento HTML
+        yield img.get('src')  #Obtenemos el atributo src de la etiqueta img
+        await asyncio.sleep(0.001) #Esperamos 0.001 segundos
